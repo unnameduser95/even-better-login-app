375e50148426d14602d83959f1bce9b46cb0ba7c
unnameduser95 <unnameduser95@pm.me>
2020-04-17 18:51:15 -0700

    add logo

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { Input, Text, StyleSheet, View } from 'react-native';
+import { Input, Text, StyleSheet, View, Dimensions, Image } from 'react-native';
+
+const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
+console.log(screenWidth, screenHeight);
 
 export default class Login extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello there</Text>
+      <View style={styles.container}>
+        <Image style={styles.logo} source={require('../../assets/logo.jpg')}/>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    marginTop: screenWidth * 0.30,
+    height: 100,
+  }
+});
