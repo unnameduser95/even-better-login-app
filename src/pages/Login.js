bf532c457c3a64e995460c6a54f6d79cc4880da2
unnameduser95 <unnameduser95@pm.me>
2020-04-17 19:07:08 -0700

    add buttons

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TextInput, Text, StyleSheet, View, Dimensions, Image } from 'react-native';
+import { TextInput, Text, StyleSheet, View, Dimensions, Image, TouchableOpacity } from 'react-native';
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 const screenHeight = Math.round(Dimensions.get('window').height);
@@ -12,6 +12,15 @@ export default class Login extends Component {
         <Image style={styles.logo} source={require('../../assets/logo.jpg')}/>
         <TextInput style={styles.field} placeholder={"Username"}></TextInput>
         <TextInput style={styles.field} placeholder={"Password"}></TextInput>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Sign in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.smallButton}>
+          <Text style={styles.smallButtonText}>Forgot password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.smallButton}>
+          <Text style={styles.smallButtonText}>Don't have an account? Sign up</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -32,5 +41,26 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: "gray",
     borderWidth: 1,
+  },
+  button: {
+    marginTop: 50,
+    marginBottom: 20,
+    borderRadius: 5,
+    height: 40,
+    width: 100,
+    backgroundColor: '#981ceb',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: 'white'
+  },
+  smallButton: {
+    height: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  smallButtonText: {
+    color: "gray"
   }
 });
