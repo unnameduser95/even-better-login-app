ab07c11aebf059508e857924ef2752b7b891b841
unnameduser95 <unnameduser95@pm.me>
2020-04-21 20:34:02 -0700

    start on register page

---
+++ src/pages/Register.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { TouchableOpacity, Text, View } from 'react-native';
+
+export default function Register(props) {
+  return (
+    <Text>Register page</Text>
+  )
+}
