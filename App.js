089e6781ed43b3d95132e649862dbbebc025f230
unnameduser95 <unnameduser95@pm.me>
2020-04-21 20:33:53 -0700

    implement navigation

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 import Login from './src/pages/Login';
+import Register from './src/pages/Register';
+
+// const screenWidth = Math.round(Dimensions.get('window').width);
+// const screenHeight = Math.round(Dimensions.get('window').height);
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Login/>
-    </View>
+    <AppearanceProvider>
+      <NavigationContainer>
+        <Stack.Navigator 
+          headerMode={"none"}
+        >
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AppearanceProvider>
   );
 }
 
 // const styles = StyleSheet.create({
 //   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
 //     alignItems: 'center',
 //     justifyContent: 'center',
+//     flex: 1,
 //   },
 // });
