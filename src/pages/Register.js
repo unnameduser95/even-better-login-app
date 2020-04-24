1948c8f9fc56de354d12d59d30cc03fda6b793e2
unnameduser95 <unnameduser95@pm.me>
2020-04-23 21:32:33 -0700

    add buttons

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -12,11 +12,18 @@ export default function Register(props) {
     // <Text>Register page</Text>
 
     <KeyboardAvoidingView style={styles.container} behavior={"padding"} enabled >
+      <Text style={styles.heading}>Sign Up</Text>
       <NameField placeholder={"First Name"} />
       <NameField placeholder={"Last Name"} />
       <EmailField />
       <NewPasswordField />
       <NewPasswordField placeholder={"Confirm Password"} />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.smallButton}>
+        <Text style={styles.smallButtonText}>Already have an account? Sign in</Text>
+      </TouchableOpacity>
     </KeyboardAvoidingView>
   )
 }
@@ -27,5 +34,31 @@ const styles = StyleSheet.create({
     width: screenWidth,
     alignItems: "center",
     justifyContent: "center",
+  },
+  heading: {
+    fontWeight: "bold",
+    fontSize: 25,
+    marginBottom: 20
+  },
+  button: {
+    marginTop: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+    height: 40,
+    width: 100,
+    backgroundColor: '#981ceb',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: 'white'
+  },
+  smallButton: {
+    height: 30,
+    justifyContent: "center",
+  },
+  smallButtonText: {
+    color: "gray",
+    fontWeight: "bold",
   }
 })
