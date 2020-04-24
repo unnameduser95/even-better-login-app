7432da90514a05a91e692f136493c87bbee3fbe9
unnameduser95 <unnameduser95@pm.me>
2020-04-23 21:18:36 -0700

    add name field and new password field

--- src/components/Fields.js
+++ src/components/Fields.js
@@ -2,6 +2,27 @@ import React, { useState } from 'react';
 import { TextInput, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
+const NameField = (props) => {
+  return (
+    <View style={styles.fieldContainer}>
+      <TextInput style={[styles.field, styles.inputContainer]}
+        placeholder={"Name"}
+        // placeholderTextColor={props.placeholderTextColor ? props.placeholderTextColor : "gray"}
+        autoCorrect={false}
+        autoCapitalize={"words"}
+        textContentType={"name"}
+        onChangeText={props.onChangeText ? props.onChangeText : () => null}
+        onSubmitEditing={props.onSubmitEditing ? props.onSubmitEditing : () => null}
+        keyboardType={"default"}
+        returnKeyType={"none"}
+        autoFocus={false}
+        blurOnSubmit={false}>
+      </TextInput>
+      <Text style={styles.errorText}>{props.message ? props.message : ""}</Text>
+    </View>
+  )
+}
+
 const EmailField = (props) => {
   // console.log(props);
   return (
@@ -15,8 +36,8 @@ const EmailField = (props) => {
         onChangeText={props.onChangeText ? props.onChangeText : () => null}
         onSubmitEditing={props.onSubmitEditing ? props.onSubmitEditing : () => null}
         keyboardType={"email-address"}
-        returnKeyType={"next"}
-        autoFocus={true}
+        returnKeyType={"none"}
+        autoFocus={props.autoFocus ? props.autoFocus : false}
         blurOnSubmit={false}>
       </TextInput>
       <Text style={styles.errorText}>{props.message ? props.message : ""}</Text>
@@ -36,6 +57,35 @@ const PasswordField = (props) => {
           autoCorrect={false}
           autoCapitalize={"none"} 
           textContentType={"password"}
+          secureTextEntry={hidePassword}
+          onChangeText={props.onChangeText ? props.onChangeText : () => null}
+          onSubmitEditing={props.onSubmitEditing ? props.onSubmitEditing : () => null}
+          focus={props.focus ? props.focus : false}>
+        </TextInput>
+        <TouchableOpacity 
+          style={styles.showHideButton}
+          onPress={() => setHidePassword(!hidePassword)}
+        >
+          <Ionicons name={hidePassword === true ? "md-eye" : "md-eye-off"} color="gray" size={22} />
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.errorText}>{props.message ? props.message : ""}</Text>
+    </View>
+  )
+}
+
+const NewPasswordField = (props) => {
+  const [hidePassword, setHidePassword] = useState(true);
+
+  return (
+    <View style={styles.fieldContainer}>
+      <View style={[styles.passwordContainer, styles.inputContainer]}>
+        <TextInput 
+          style={[styles.field, {width: 235}]}  // custom width for password field; must have space for show/hide button (width 40)
+          placeholder={"Password"} 
+          autoCorrect={false}
+          autoCapitalize={"none"} 
+          textContentType={"newPassword"}
           secureTextEntry={hidePassword}
           onChangeText={props.onChangeText ? props.onChangeText : () => null}
           onSubmitEditing={props.onSubmitEditing ? props.onSubmitEditing : () => null}
