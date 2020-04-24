4e306b5d7db5e98b0773f9aff2449942844ad069
unnameduser95 <unnameduser95@pm.me>
2020-04-23 21:25:19 -0700

    add fields

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import { TouchableOpacity, Text, View } from 'react-native';
+import { TouchableOpacity, Text, View, Dimensions, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { ScreenStackHeaderLeftView } from 'react-native-screens';
+
+import { NameField, EmailField, NewPasswordField } from '../components/Fields';
+
+const screenWidth = Math.round(Dimensions.get('screen').width);
+const screenHeight = Math.round(Dimensions.get('screen').height);
 
 export default function Register(props) {
   return (
-    <Text>Register page</Text>
+    // <Text>Register page</Text>
+
+    <KeyboardAvoidingView style={styles.container} behavior={"padding"} enabled >
+      <NameField placeholder={"First Name"} />
+      <NameField placeholder={"Last Name"} />
+      <EmailField />
+      <NewPasswordField />
+      <NewPasswordField />
+    </KeyboardAvoidingView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: screenHeight,
+    width: screenWidth,
+    alignItems: "center",
+    justifyContent: "center",
+  }
+})
