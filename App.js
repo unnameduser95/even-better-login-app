a6b82481f831f2c23247bdedbf862956869fc11e
unnameduser95 <unnameduser95@pm.me>
2020-04-24 18:47:33 -0700

    remove unnecessary libraries

--- App.js
+++ App.js
@@ -1,36 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from './src/pages/Login';
 import Register from './src/pages/Register';
 
-// const screenWidth = Math.round(Dimensions.get('window').width);
-// const screenHeight = Math.round(Dimensions.get('window').height);
-
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <AppearanceProvider>
-      <NavigationContainer>
-        <Stack.Navigator 
-          headerMode={"none"}
-        >
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AppearanceProvider>
+    <NavigationContainer>
+      <Stack.Navigator 
+        headerMode={"none"}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     flex: 1,
-//   },
-// });
